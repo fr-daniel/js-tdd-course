f421dd252adac5f15ae2bf1a1fdb7fb076f8e369
Daniel Lima <fr.daniel.lima@gmail.com>
2018-01-14 12:19:51 -0300

    Add ESLint

---
+++ src/main.js
@@ -0,0 +1,12 @@
+const a = 3;
+
+const b = 2;
+
+const c = 'jaaaaaaaaaaaaaaaaaaaaalslsllsslsllsslslaaaaaaaaaaaajajajajajajajajajajkskskskskksksksksskskskskskskskskskajajaajaj';
+
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
